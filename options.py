08f79c8f3726b1ad8fc6a8f441323b9ae9818296
hioikawa <101631407+uminomae@users.noreply.github.com>
2023-12-12 03:39:53 +0900

    Add files via upload

--- options.py
+++ options.py
@@ -1,15 +1,26 @@
 window_width = 230 
 window_height = 400 
 
+DATA_WINDOW_SIZE = 6
+
 # デフォルトのグラフタイトルとデータスパン
 DEFAULT_GRAPH_TITLE = "Muse2"
 DEFAULT_DATA_SPAN = "60"
 
-TITLE_OPTIONS = ['meditation', 'meditation deep', 'sleeping', 'coding', 'gaming(TPS']
+TITLE_OPTIONS = ['meditation', 'meditation deep', 'sleeping', 'coding', 'gaming(TPS)']
 # 抽出されたデータのディレクトリ名
-EXTRACTED_DATA_DIR = "extracted_data"
+EXTRACTED_DATA_DIR = "data/extracted_data"
 # 保存するディレクトリのパス
-SAVE_DIRECTORY = 'saved_plots'
+SAVE_DIRECTORY = 'data/saved_plots'
+
+REQUIRED_COLUMNS = [
+    'Delta_TP9', 'Delta_AF7', 'Delta_AF8', 'Delta_TP10',
+    'Theta_TP9', 'Theta_AF7', 'Theta_AF8', 'Theta_TP10',
+    'Alpha_TP9', 'Alpha_AF7', 'Alpha_AF8', 'Alpha_TP10',
+    'Beta_TP9', 'Beta_AF7', 'Beta_AF8', 'Beta_TP10',
+    'Gamma_TP9', 'Gamma_AF7', 'Gamma_AF8', 'Gamma_TP10',
+    'RAW_TP9', 'RAW_AF7', 'RAW_AF8', 'RAW_TP10',
+]
 
 COLORS = {
 	"window_background": "#f5f5dc",  # ベージュの背景色
